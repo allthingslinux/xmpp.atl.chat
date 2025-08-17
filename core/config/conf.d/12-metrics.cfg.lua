90ab4b14f23312b3e354969ac2a3f6da5bed43cb
Logan Honeycutt <admin@kzn.sh>
2025-08-17 11:12:22 -0400

    feat(metrics): add PROSODY_METRICS_PUBLIC toggle; default allow Docker bridge CIDR, keep loopback IPs

--- core/config/conf.d/12-metrics.cfg.lua
+++ core/config/conf.d/12-metrics.cfg.lua
@@ -5,6 +5,7 @@
 statistics = "internal"
 statistics_interval = Lua.os.getenv("PROSODY_STATISTICS_INTERVAL") or "manual"
 
+-- By default restrict to loopback; allow-list is expanded via CIDR below
 openmetrics_allow_ips = {
 	"127.0.0.1",
 	"::1",
@@ -21,6 +22,12 @@ if metrics_ips_env then
 	end
 end
 
-openmetrics_allow_cidr = Lua.os.getenv("PROSODY_METRICS_ALLOW_CIDR") or "172.16.0.0/12"
+-- Public exposure toggle (set PROSODY_METRICS_PUBLIC=true to expose)
+local metrics_public = (Lua.os.getenv("PROSODY_METRICS_PUBLIC") == "true")
+if metrics_public then
+	openmetrics_allow_cidr = "0.0.0.0/0"
+else
+	openmetrics_allow_cidr = Lua.os.getenv("PROSODY_METRICS_ALLOW_CIDR") or "172.16.0.0/12"
+end
 
 
